2f7e0bdffaa9ccd71bca3ac289a743f0e54757fe
WilsonParker <zerad3208@gmail.com>
2024-08-27 12:25:24 +0900

    Update mission.dart
    add get cookie
    test shopping, user api

--- lib/mission.dart
+++ lib/mission.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -36,10 +37,13 @@ class _MissionState extends State<Mission> {
     super.initState();
 
     _controller = WebViewController()
+      ..setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
+      ..clearLocalStorage()
+      ..clearCache()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..addJavaScriptChannel("Channel",
           onMessageReceived: (JavaScriptMessage message) {
-        debugPrint('''
+            dPrint('''
           open mission
           message received ${message.message}
         ''');
@@ -52,14 +56,16 @@ class _MissionState extends State<Mission> {
             // Update loading bar.
           },
           onPageStarted: (String url) {
-            debugPrint('''
+            dPrint('''
               page start $url
             ''');
           },
           onPageFinished: (String url) {
-            debugPrint('''
+            dPrint('''
               page finish $url
             ''');
+
+            getShoppingPage(_controller);
 
             String currentStepScript = _steps[_step];
             String currentStepUrl = getArchitectureValue(
@@ -67,7 +73,7 @@ class _MissionState extends State<Mission> {
             RegExp regExp = RegExp(r"^" + currentStepUrl);
 
             if (regExp.hasMatch(url)) {
-              debugPrint('''
+              dPrint('''
                 match
               ''');
               String script =
@@ -77,7 +83,7 @@ class _MissionState extends State<Mission> {
             }
           },
           onWebResourceError: (WebResourceError error) {
-            debugPrint('''
+            dPrint('''
               Page resource error:
               code: ${error.errorCode}
               description: ${error.description}
@@ -86,18 +92,33 @@ class _MissionState extends State<Mission> {
           ''');
           },
           onNavigationRequest: (NavigationRequest request) {
+            dPrint('navigation to ${request.url}');
             if (request.url.startsWith('https://www.youtube.com/')) {
-              debugPrint('blocking navigation to ${request.url}');
               return NavigationDecision.prevent;
             }
-            debugPrint('allowing navigation to ${request.url}');
+            dPrint('allowing navigation to ${request.url}');
             return NavigationDecision.navigate;
           },
           onHttpError: (HttpResponseError error) {
-            debugPrint('Error occurred on page: ${error.response?.statusCode}');
+            dPrint('Error occurred on page: ${error.response?.statusCode}');
           },
           onUrlChange: (UrlChange change) {
-            debugPrint('url change to ${change.url}');
+            dPrint('url change to ${change.url}');
+
+            RegExp regExp = RegExp(r"^https://m.place.naver.com");
+
+            if (regExp.hasMatch(change.url.toString())) {
+              dPrint('''
+                change match
+              ''');
+
+              Timer(Duration(seconds: 2), () {
+                String script =
+                buildImport(
+                    getArchitectureValue('tab.map.sub-tabs.parking.script'));
+                _controller.runJavaScript(script);
+              });
+            }
           },
           onHttpAuthRequest: (HttpAuthRequest request) {
             // openDialog(request);
@@ -125,6 +146,61 @@ class _MissionState extends State<Mission> {
       // then throw an exception.
       throw Exception('Failed to load architecture');
     }
+  }
+
+  Future<Object> getCookie(WebViewController controller)  async {
+    final cookies = await controller.runJavaScriptReturningResult(
+      'document.cookie',
+    );
+    return cookies;
+  }
+
+  Future<Map<String, dynamic>> getShoppingPage(WebViewController controller) async {
+    return getCookie(controller).then((cookie) async{
+      // String url = "https://new-m.pay.naver.com/api/timeline/v2/advanced-search?serviceCategory=SHOPPING&requestUrl=https://new-m.pay.naver.com/historybenefit/paymenthistory?serviceCategory=SHOPPING&from=MOBILE_PAYMENT_HISTORY";
+      String url = "https://new-m.pay.naver.com/api/common/member";
+      // String url = "https://m.naver.com/preview/index.json?bizTalk=yes";
+
+      final response = await http.get(Uri.parse(url),
+          headers: <String, String> {
+            // "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+            // "Referer" : "https://new-m.pay.naver.com/pcpay?page=1",
+            // "Content-Type" : "application/json; charset=utf-8",
+            // "Cookie" : cookie.toString()
+            // "Cookie" : "NID_AUT=ApyJpw364NGvU4/Cf8gmceTVSM2fnc9QIrAflh7zVLBE/xGFbW8unbkXQbW85BVO; NID_SES=AAABgAuR/fvNLMycIyliwJf+PYU40uJ211j8G6xYq5gyUmZ6DJ/7Z0J/u57OppVj+5vusmO4OOu/vNyNb4bwJu3NdfzUqJv55ofIGR8wH1bTzbvrM3dv1ma4IoRUh4j/yHbAdiiAFLKTQcLBbRhH196U3+qjzpnnNsdgFIQn5MBBi9W2qHZpC94KdepP6Isch+vJqQSGdj4t5CiGXQ7PLrruhX3RYpmbQOfHaPujM5w1XwIQydqGVWSSdvb4zAOVsDaRap/1sjn4Jo2vOz7RWF1+6BnsGM9GftHLsdDY74kdA5Ultm5S/JGpXHBt6WkM51XUiGscjvusXAnrrcRsriF+gN6X9g9Gifn2uoHopKMU6vLCQSMSMWasc9SD2bClmFyukb3LpznKZGky2W4v1x8DzqhyOqrYVv9X5xUIJC/e45yaEu+gFpR4ymwRQCcR2F0YtMT/DrOQ7oOnHPN7PJv3olm41roi6eg6+jCganbeAgsXAEv9PJ+aLCwUq8kLLLOznw==;"
+            "Cookie" : "NID_SES=AAABgpvBmNZwf8cLM6895JlDtbNMtdICrW7rgFYac9A5J0XsS/cuFQQoFCDrGnoteExkGNQucSzdeHwWvaaKEEIvU5DbwcAHC1uA6v/buCS1Fmuhc7vX7V288x8u5hu031uS8JsDe/dqJzLqcIJdfOa9VrzUF4EJs5BpOPR+PbuiY9fMkuB9LU5CjI+88oPF6Jsh6OfBmVUSTmcqyOE7DEjddjdZF2ABwse6v+FxxuP8Q7hUSayq+5eTqqi3d9QRC+1afccC55i7bcZvZRFvnalkT5lNN7QjPSocDMKboYewbHPAKWd/vNjedLO+KJVrEAFwK6GsbAeo54yhlw5UPVaMgBBiwGXghm/7P8Ur+HbCEsZ7c4JCjk2qlMaWG6W8Ne9+rbMlp036SFEcPBJDEKVinF/IyjRQjAsLD8+FjRo/PyoYFQ9jhHYno/R/BBTMmA1HoxHGWukYwfycUKnNgVbhRS735u15hQI03wj88F0hLXJEd8E73uwxra8RWGgCs1GHhQyXqvMjSItmNQi9V9VujrI=;"
+          });
+
+      dPrint('''
+      cookie ${cookie.toString()}
+    ''');
+
+      // NAC=v2GpBQwzXwmQ; NNB=5YU3IWSPFHGWM; NACT=1; nid_inf=271612173; NID_AUT=ApyJpw364NGvU4/Cf8gmceTVSM2fnc9QIrAflh7zVLBE/xGFbW8unbkXQbW85BVO; NID_SES=AAABgAuR/fvNLMycIyliwJf+PYU40uJ211j8G6xYq5gyUmZ6DJ/7Z0J/u57OppVj+5vusmO4OOu/vNyNb4bwJu3NdfzUqJv55ofIGR8wH1bTzbvrM3dv1ma4IoRUh4j/yHbAdiiAFLKTQcLBbRhH196U3+qjzpnnNsdgFIQn5MBBi9W2qHZpC94KdepP6Isch+vJqQSGdj4t5CiGXQ7PLrruhX3RYpmbQOfHaPujM5w1XwIQydqGVWSSdvb4zAOVsDaRap/1sjn4Jo2vOz7RWF1+6BnsGM9GftHLsdDY74kdA5Ultm5S/JGpXHBt6WkM51XUiGscjvusXAnrrcRsriF+gN6X9g9Gifn2uoHopKMU6vLCQSMSMWasc9SD2bClmFyukb3LpznKZGky2W4v1x8DzqhyOqrYVv9X5xUIJC/e45yaEu+gFpR4ymwRQCcR2F0YtMT/DrOQ7oOnHPN7PJv3olm41roi6eg6+jCganbeAgsXAEv9PJ+aLCwUq8kLLLOznw==; NID_JKL=/aXpJc8Y/fOzt3SbwyOV2wNVo6aQ74DcCitR7ITyZII=; BUC=0BXZbZomzwpsSX6kV1NkwReMJAiNKQw4dBicW1tqdpQ=
+      // NNB=CQAR4TKLPHCWM; MM_PF=SEARCH; BNB_FINANCE_HOME_TOOLTIP_PAYMENT=true; BNB_FINANCE_HOME_TOOLTIP_ESTATE=true; BNB_FINANCE_HOME_TOOLTIP_STOCK=true; NID_JKL=gJV6KYBQVZuSjuHj1g69QudYOonfpyIQRPMZ55ev1uo=; NID_SES=AAABgpvBmNZwf8cLM6895JlDtbNMtdICrW7rgFYac9A5J0XsS/cuFQQoFCDrGnoteExkGNQucSzdeHwWvaaKEEIvU5DbwcAHC1uA6v/buCS1Fmuhc7vX7V288x8u5hu031uS8JsDe/dqJzLqcIJdfOa9VrzUF4EJs5BpOPR+PbuiY9fMkuB9LU5CjI+88oPF6Jsh6OfBmVUSTmcqyOE7DEjddjdZF2ABwse6v+FxxuP8Q7hUSayq+5eTqqi3d9QRC+1afccC55i7bcZvZRFvnalkT5lNN7QjPSocDMKboYewbHPAKWd/vNjedLO+KJVrEAFwK6GsbAeo54yhlw5UPVaMgBBiwGXghm/7P8Ur+HbCEsZ7c4JCjk2qlMaWG6W8Ne9+rbMlp036SFEcPBJDEKVinF/IyjRQjAsLD8+FjRo/PyoYFQ9jhHYno/R/BBTMmA1HoxHGWukYwfycUKnNgVbhRS735u15hQI03wj88F0hLXJEd8E73uwxra8RWGgCs1GHhQyXqvMjSItmNQi9V9VujrI=; nid_inf=271601234; page_uid=iV03ulpr4boss7xvVsVssssstYV-385707; NAC=z41LBQQsdQOG; NACT=1; MM_search_homefeed=true
+      if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      // return MissionDTO.fromJson(jsonDecode(response.body));
+      dPrint('''
+        getShoppingPage ${response.body}
+      ''');
+      dPrint('''
+        getShoppingPage ${response.headers}
+      ''');
+      dPrint('''
+        getShoppingPage ${response.headers['set-cookie']}
+      ''');
+      return jsonDecode(response.body);
+      } else {
+      dPrint('''
+        getShoppingPage ${response.statusCode}
+        getShoppingPage ${response.body}
+      ''');
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load api');
+      }
+    });
   }
 
   /// code 에 해당되는 값을 _json 에서 가져옵니다
@@ -159,10 +235,12 @@ class _MissionState extends State<Mission> {
     return text;
   }
 
+  dPrint(String $message) {
+    debugPrint($message);
+  }
+
   injectJavascript(WebViewController controller, String script) async {
-    debugPrint('''
-      injectJavascript
-    ''');
+    dPrint("injectJavascript");
     controller.runJavaScript('''
     ''');
   }
